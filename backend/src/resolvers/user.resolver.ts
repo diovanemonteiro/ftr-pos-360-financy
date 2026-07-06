0bd5a4224eab56699e747e6b062ea18c54c9096f
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 16:51:44 -0400

    Adiciona operação `createUser` ao resolver de usuário no backend.

--- backend/src/resolvers/user.resolver.ts
+++ backend/src/resolvers/user.resolver.ts
@@ -1,13 +1,21 @@
-import {Arg, Query, Resolver, UseMiddleware} from "type-graphql";
+import {Arg, Mutation, Query, Resolver, UseMiddleware} from "type-graphql";
 import {UserModel} from "../models/user.model";
 import {UserService} from "../services/user.service";
 import { IsAuth } from "../middlewares/auth.middleware";
+import {CreateUserInput} from "../dtos/input/user.input";
 
 @Resolver(() => UserModel)
 @UseMiddleware(IsAuth)
 export class UserResolver {
 
     private userService = new UserService()
+
+    @Mutation(() => UserModel)
+    async createUser(
+        @Arg('input', () => CreateUserInput) input: CreateUserInput
+    ):Promise<UserModel> {
+        return this.userService.createUser(input)
+    }
 
     @Query(() => UserModel)
     async getUser(
