6b38265b844ef2708444a1f93f0aad7117e84086
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 14:10:04 -0400

    Adiciona classe `LoginInput` ao DTO de autenticação no backend.

--- backend/src/dtos/input/auth.input.ts
+++ backend/src/dtos/input/auth.input.ts
@@ -1,6 +1,15 @@
 import { InputType, Field } from "type-graphql";
 
 @InputType()
+export class LoginInput {
+    @Field(() => String)
+    email!: string
+
+    @Field(() => String)
+    password!: string
+}
+
+@InputType()
 export class RegisterInput {
     @Field(() => String)
     name!: string
