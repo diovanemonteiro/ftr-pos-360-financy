f5ce7fa1e1ea9950620446fb8653f3e51445510b
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 14:09:21 -0400

    Adiciona função `comparePassword` ao utilitário de hash no backend.

--- backend/src/utils/hash.ts
+++ backend/src/utils/hash.ts
@@ -4,3 +4,10 @@ export const hashPassword = async (password: string): Promise<string> => {
     const salt = await bcrypt.genSalt(10);
     return bcrypt.hash(password, salt);
 }
+
+export const comparePassword = (
+    plainPassword: string,
+    hashPassword: string
+) => {
+    return bcrypt.compare(plainPassword, hashPassword)
+}
