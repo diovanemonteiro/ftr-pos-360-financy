03769019341d697cca8141f75c65256ceaf9f156
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 16:55:39 -0400

    Adiciona migration para criação da tabela `Category` no schema Prisma no backend.

---
+++ backend/prisma/migrations/20260706190538_create_category/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "color" TEXT,
+    "type" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" TEXT NOT NULL,
+    CONSTRAINT "Category_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
