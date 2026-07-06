5121e778f5ba76321441259c8b62fae53874e13b
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 14:11:13 -0400

    Configura CORS e adiciona criação de contexto ao middleware GraphQL no backend.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,26 @@
 import 'reflect-metadata'
 import express from 'express'
+import cors from 'cors'
 import { ApolloServer } from '@apollo/server'
 import { buildSchema } from "type-graphql";
 import { expressMiddleware } from "@as-integrations/express5";
-
 import { AuthResolver } from "./resolvers/auth.resolver";
-import {UserResolver} from "./resolvers/user.resolver";
+import { UserResolver } from "./resolvers/user.resolver";
+import { buildContext } from "./graphql/context";
 
 async function bootstrap() {
     const app = express()
 
+    app.use(cors({
+        origin: 'localhost:5173',
+        credentials: true,
+    }))
+
     const schema = await buildSchema({
-        resolvers: [AuthResolver, UserResolver],
+        resolvers: [
+            AuthResolver,
+            UserResolver
+        ],
         validate: false,
         emitSchemaFile: './schema.graphql',
     })
@@ -22,7 +31,13 @@ async function bootstrap() {
 
     await server.start()
 
-    app.use('/graphql', express.json(), expressMiddleware(server))
+    app.use(
+        '/graphql',
+        express.json(),
+        expressMiddleware(server, {
+            context: buildContext,
+        })
+    )
 
     app.listen({
         port: 4000
