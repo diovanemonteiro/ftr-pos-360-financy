65ad1455e19a515f43062561a7e13368db38a58c
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 14:09:41 -0400

    Adiciona função `login` ao serviço de autenticação no backend.

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,10 +1,22 @@
 import {prismaClient} from "../../prisma/prisma";
 import type {RegisterInput} from "../dtos/input/auth.input";
-import {hashPassword} from "../utils/hash";
+import {comparePassword, hashPassword} from "../utils/hash";
 import {signJwt} from "../utils/jwt";
 import type {UserModel} from "../models/user.model";
+import {LoginInput} from "../dtos/input/auth.input";
 
 export class AuthService {
+
+    async login(data: LoginInput) {
+        const existingUser = await prismaClient.user.findUnique({
+            where: { email: data.email },
+        })
+        if (!existingUser) throw new Error('Usuário não cadastrado!')
+        const compare = await comparePassword(data.password, existingUser.password)
+        if (!compare) throw new Error('Senha inválida!')
+        return this.generateToken(existingUser)
+    }
+
     async register(data: RegisterInput) {
 
         const existingUser = await prismaClient.user.findUnique({
