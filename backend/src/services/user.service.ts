628fca94c922348ccaaebcf4612a26b3e7be378c
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-06 16:52:35 -0400

    Adiciona função `createUser` ao serviço de usuário no backend.

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,6 +1,26 @@
 import {prismaClient} from "../../prisma/prisma";
+import {CreateUserInput} from "../dtos/input/user.input";
 
 export class UserService {
+
+    async createUser(input: CreateUserInput) {
+
+        const findUser = prismaClient.user.findUnique({
+            where: {
+                email: input.email
+            }
+        })
+
+        if (findUser) throw new Error('Usuario ja existe')
+
+        return await prismaClient.user.create({
+            data: {
+                name: input.name,
+                email: input.email
+            }
+        })
+    }
+
     async findUser(id: string) {
         const user = await prismaClient.user.findUnique({
             where: {
