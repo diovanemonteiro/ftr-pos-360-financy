15836d823b5857675ece22fa02d1fc2ac7bc22d7
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-07-08 14:40:05 -0400

    Remove `useState` and unused assets from `App.tsx`. Refactor imports for cleanup.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import {Login} from "@/pages/Login.tsx";
 import {Register} from "@/pages/Register.tsx";
 import {Layout} from "@/components/Layout";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Layout>
