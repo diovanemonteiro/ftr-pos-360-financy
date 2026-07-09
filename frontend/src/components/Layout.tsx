644833bd2185d71b3322db49cc5c0a8efa0594a0
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-07-09 15:25:38 -0400

    Adiciona componente `Header` ao layout principal do frontend.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from '@/components/ui/sonner'
+import {Header} from "@/components/Header";
 
 interface LayoutProps {
   children: React.ReactNode
@@ -8,6 +9,7 @@ export function Layout({ children }: LayoutProps) {
   return (
     <>
         <div className="min-h-screen bg-gray-100">
+            <Header />
             <main className="mx-auto px-16 py-4">
                 {children}
             </main>
