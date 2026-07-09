4a197164c5ad8c46cb71247984180f52118e01a4
Diovane Monteiro <diovane.sousa@manaus.am.gov.br>
2026-07-09 09:15:25 -0400

    Adiciona mutations e query para gerenciamento de categorias no GraphQL

---
+++ frontend/src/lib/graphql/queries/Category.ts
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client'
+
+export const LIST_CATEGORIES = gql`
+  query ListCategories {
+    listCategories {
+      id
+      name
+      color
+      type
+      userId
+      createdAt
+      updatedAt
+    }
+  }
+`
