26716700a572bf5fb89a2697a6b1d217357bcc40
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-09 19:27:14 -0400

    Adiciona rota protegida para a página de transações.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import {Register} from "@/pages/Auth/Register.tsx";
 import {Layout} from "@/components/Layout";
 import {useAuthStore} from "@/stores/auth";
 import {Dashboard} from "@/pages/Dashboard";
+import {Transactions} from "@/pages/Transactions";
 import {Categories} from "@/pages/Categories";
 
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
@@ -24,6 +25,7 @@ function App() {
               <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
               <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
               <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+              <Route path="/transactions" element={<ProtectedRoute><Transactions /></ProtectedRoute>} />
               <Route path="/categories" element={<ProtectedRoute><Categories /></ProtectedRoute>} />
           </Routes>
       </Layout>
