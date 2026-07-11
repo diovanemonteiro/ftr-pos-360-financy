38f5b56ef1a965515312fe85a0b523a4b24cd872
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-11 00:21:35 -0400

    Refina estilos do componente `Page`, ajustando bordas para `rounded-lg` e adicionando borda cinza clara.

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -4,7 +4,7 @@ interface PageProps {
 
 export function Page({ children }: PageProps) {
   return (
-    <div className="min-h-[calc(100vh-9rem)] bg-white rounded-xl p-8 mt-4">
+    <div className="min-h-[calc(100vh-9rem)] bg-white rounded-lg p-8 mt-4 border border-gray-200">
       {children}
     </div>
   )
