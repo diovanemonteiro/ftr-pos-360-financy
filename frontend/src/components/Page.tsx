ee273f37d993804c7b01d89e4f04c78d37cda80a
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-11 16:08:54 -0400

    Ajusta padding e remove bordas no componente `Page` para simplificar estilos.

--- frontend/src/components/Page.tsx
+++ frontend/src/components/Page.tsx
@@ -4,7 +4,7 @@ interface PageProps {
 
 export function Page({ children }: PageProps) {
   return (
-    <div className="min-h-[calc(100vh-9rem)] bg-white rounded-lg p-8 mt-4 border border-gray-200">
+    <div className="min-h-[calc(100vh-9rem)] p-12">
       {children}
     </div>
   )
