837582dce50797f7b60f8f29b998623e06576f9e
Diovane Monteiro <diovanemonteiro@gmail.com>
2026-07-14 23:24:24 -0400

    Remove os campos `title` e `date` da tabela `Transaction` e torna `description` obrigatório.

--- backend/prisma/migrations/20260706220407_create_transactions/migration.sql
+++ backend/prisma/migrations/20260706220407_create_transactions/migration.sql
@@ -1,11 +1,9 @@
 -- CreateTable
 CREATE TABLE "Transaction" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "amount" REAL NOT NULL,
     "type" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "description" TEXT,
+    "description" TEXT NOT NULL,
+    "amount" REAL NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
     "userId" TEXT NOT NULL,
